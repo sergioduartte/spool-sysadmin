d7148dfad754b7e8b68ec70fc011d9cb1d9d94c8
sergioduartte <sduartecc@gmail.com>
2018-12-11 23:57:20 -0300

    imprimindo e gerando log mensal, falta add ao cron...

--- scripts/gen-history.sh
+++ scripts/gen-history.sh
@@ -5,7 +5,9 @@
 mes=`date +%b-%Y`
 
 touch ../data/$mes.txt
+header=`head -n1 ../data/current.txt | awk -F"\t" '{print $1,FS,FS,$5,FS,$6}'`
+echo -e "$header" > ../data/$mes.txt
+sed '1d' ../data/current.txt  | awk -F"\t" '{print $1,FS,$4,FS,$5}' | sort >> ../data/$mes.txt
 
-sed -n '1p' ../data/current.txt | awk -F"\t" '{print $1,FS,FS,$5,FS,$6}' > ../data/$mes.txt
-sed '1d' ../data/current.txt  | awk -F"\t" '{print $1,FS,$4,FS,$5}' >> ../data/$mes.txt
 
+echo "relatorio de" $mes "gerado com sucesso"
--- scripts/print.sh
+++ scripts/print.sh
@@ -27,7 +27,6 @@ if [ $fileSize -lt 1 ]; then
 		fileSize=1 
 fi
 newPrintedPages=$(($fileSize + $userPrinted))
-echo $newPrintedPages "oxe..."
 
 overflowQuota=0
 
@@ -35,7 +34,6 @@ overflowQuota=0
 if [ $newPrintedPages -gt 30 ]; then
 	overflowQuota=$(($newPrintedPages - 30))
 	newPrintedPages=30
-	echo "to passando aqui"
 fi
 
 ## atualiza o bd
@@ -45,4 +43,4 @@ sed -i "/$user/d" ../data/current.txt
 echo -e $newString >> ../data/current.txt
 
 # imprimir quanto de cota resta ao usuario
-echo "o usuario ainda tem "$(bash get-quota.sh $user) "paginas de cota"
+echo "restam "$(bash get-quota.sh $user) "paginas de cota mensal"
