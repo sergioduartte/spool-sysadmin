62544181e6b5340f961373cb63891897efa56d65
sergioduartte <sduartecc@gmail.com>
2018-12-12 08:26:43 -0300

    deu ruim

--- scripts/update-history.sh
+++ scripts/update-history.sh
@@ -3,6 +3,18 @@
 # atualiza os arquivos current.txt, last.txt baseados no consumo mensal.
 
 # gera o arquivo mensal
+# current -> mes-ano.txt
 bash gen-history.sh  
 
 
+# o last eh utilizado como um arquivo auxiliar somente na hora de atualizar o consumo
+## last -> current	
+#header=`head -n1 ../data/current.txt`
+#echo -e "$header" > ../data/current.txt
+#sed '1d' ../data/current.txt  | awk -F"\t" '{print $1,FS,$4,FS,$5}' | sort >> ../data/$mes.txt
+
+
+
+
+
+echo "ready to work!!"
